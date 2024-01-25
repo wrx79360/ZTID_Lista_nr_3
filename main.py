1f66ed803a588ca90007344853ee1c70e3142b57
wrx79360 <wrx79360@student.wroclaw.merito.pl>
2024-01-25 18:15:07 +0100

    firt commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Lista nr 3
+# Filip Kuś 85992
+import base64
+
+
+def encode_and_decode(text):
+    # Konwersja tekstu na bajty
+    text_bytes = text.encode('ascii')
+    # Szyfrowanie base64
+    encoded_string = base64.b64encode(text_bytes).decode('ascii')
+    # Odszyfrowanie base64
+    decoded_string = base64.b64decode(encoded_string).decode('ascii')
+
+    return encoded_string, decoded_string
+
+
+input_string = "Lista nr3"
+result_encode, result_decode = encode_and_decode(input_string)
+
+print(f'Text to encrypt {input_string}')
+print(f'Text encoded {result_encode}')
+print(f'Text decoded {result_decode}')
